0ab7b1cf356731735995867461d2df206736489a
AD\ejr36 <ejr36@cam.ac.uk>
2017-04-12 17:07:21 +0100

    this *should* fix errors when no candidates are sent to be tiled

--- +fsa/tile_segments.m
+++ +fsa/tile_segments.m
@@ -3,8 +3,15 @@ function tiled_segments = tile_segments(shell_segments)
 num_segments = length(shell_segments);
 num_rows = floor(sqrt(num_segments));
 num_cols = ceil(num_segments / num_rows);
-segment_size = size(shell_segments{1}, 1);
-
+% 2017_04_09 fix for avoiding problems if not candidates are found:
+if(num_segments>0)
+  segment_size = size(shell_segments{1}, 1);
+else
+	segment_size = 1; % this should stay empty but prevent index errors
+	num_rows = 1;
+	num_cols = 1;
+end
+	
 tiled_segments = zeros(num_rows * segment_size, num_cols * segment_size);
 
 for i=1:num_segments
